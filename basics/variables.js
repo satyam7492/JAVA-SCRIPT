92e84add66672893d60b3ba8f7c74921adce0d6d
satyam7492 <soni_satyam@outlook.com>
2024-11-14 13:57:02 +0530

    Update variables.js

--- basics/variables.js
+++ basics/variables.js
@@ -1,19 +1,1 @@
-const accountId = 12345
-let accountEmail = "admin"
-var password = "11111"
-city = "alld."
 
-/*
-Prefer not to use var.
-Because of issue in block scope and functional scope.
-*/
-
-
-accountEmail= "User"
-password = "22222"
-city = "srj"
-let accountState;
-
-
-console.table([accountEmail, accountId, password, city, accountState]);
- // nothing is changed don't be worry about anything //
