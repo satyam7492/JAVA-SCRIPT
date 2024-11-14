8433109d1bd9208290276c5c5920d714c7df4728
satyam7492 <soni_satyam@outlook.com>
2024-11-14 14:05:29 +0530

    Update conversion.js

--- conversion.js
+++ conversion.js
@@ -1,1 +1,13 @@
+let score = "sam"
 
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let IsLoggedIn = "1"
+let booleanIsLoggedIn = Boolean(IsLoggedIn);
+console.log(booleanIsLoggedIn);
+let SomeNumber = 30
+let StringNumber = String(SomeNumber)
+console.log(typeof StringNumber)
